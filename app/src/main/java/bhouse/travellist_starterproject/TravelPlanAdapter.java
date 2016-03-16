e9f6b15bca29d0b60b28d115cf63b9938bede471
valentinerutto <rutto28@gmail.com>
2016-03-17 01:03:07 +0300

    travelplan extend recycler adapter and implemented methods

--- app/src/main/java/bhouse/travellist_starterproject/TravelPlanAdapter.java
+++ app/src/main/java/bhouse/travellist_starterproject/TravelPlanAdapter.java
@@ -1,7 +1,31 @@
 package bhouse.travellist_starterproject;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by valentine on 3/17/16.
  */
-public class TravelPlanAdapter {
+public class TravelPlanAdapter extends RecyclerView.Adapter<TravelPlanAdapter.ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
