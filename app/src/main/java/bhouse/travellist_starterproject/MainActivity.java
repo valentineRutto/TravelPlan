6247dc3ab92c076fd953335a51c35e4b5502a0dc
valentinerutto <rutto28@gmail.com>
2016-03-17 00:29:14 +0300

    initialized recyclerview and passed staggered layout to it on main activity

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -2,6 +2,8 @@ package bhouse.travellist_starterproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,13 +13,18 @@ public class MainActivity extends Activity {
 
   private Menu menu;
   private boolean isListView;
-
+  private RecyclerView mRecyclerView;
+  private StaggeredGridLayoutManager mStaggeredLayoutManager;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     isListView = true;
+
+    mRecyclerView = (RecyclerView) findViewById(R.id.list);
+    mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+    mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
   }
 
   private void setUpActionBar() {
