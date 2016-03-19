5fd7ed246cdd10dd447ce1fc0affeb2cdb1e7542
valentinerutto <rutto28@gmail.com>
2016-03-19 20:28:26 +0300

    added a filed madapter to hold reference to the adapter

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends Activity {
   private boolean isListView;
   private RecyclerView mRecyclerView;
   private StaggeredGridLayoutManager mStaggeredLayoutManager;
+  private TravelPlanAdapter mAdapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
