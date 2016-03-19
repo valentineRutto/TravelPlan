14ecfc338b03723de92f8346b5f04ef28f8a36bf
valentinerutto <rutto28@gmail.com>
2016-03-19 20:29:57 +0300

    created an instance of the adapter and passed it to the recyclerview

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends Activity {
     mRecyclerView = (RecyclerView) findViewById(R.id.list);
     mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
     mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
+
+    mAdapter = new TravelPlanAdapter(this);
+    mRecyclerView.setAdapter(mAdapter);
   }
 
   private void setUpActionBar() {
