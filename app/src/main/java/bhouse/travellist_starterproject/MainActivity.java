8bffe2cddd84031107349e2b066f95b3a52ba750
valentinerutto <rutto28@gmail.com>
2016-03-21 23:47:09 +0300

    added intent so that when place is clicked it shows the detail layout

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -1,6 +1,7 @@
 package bhouse.travellist_starterproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -40,6 +41,10 @@ public class MainActivity extends Activity {
     @Override
     public void onItemClick(View v, int position) {
       Toast.makeText(MainActivity.this, "Clicked " + position, Toast.LENGTH_SHORT).show();
+
+      Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+      intent.putExtra(DetailActivity.EXTRA_PARAM_ID, position);
+      startActivity(intent);
     }
   };
   private void setUpActionBar() {
